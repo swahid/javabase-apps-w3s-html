96e5f9398e0c3ec72e37b5e876ac8d7f73887588
swahid <saurav1161@gmail.com>
2016-05-06 23:19:45 +0600

    06MAY2016 11:20 PM

--- app/src/main/java/org/javabase/apps/html/Home.java
+++ app/src/main/java/org/javabase/apps/html/Home.java
@@ -9,14 +9,22 @@ import android.widget.ImageView;
 public class Home extends Activity {
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        setTitle("Hello apps");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ImageView iv = (ImageView)findViewById(R.id.banner);
-        iv.setImageResource(R.mipmap.banner);
     }
 
+    private void logo(){
+        ImageView iv = (ImageView)findViewById(R.id.banner);
+        iv.setImageResource(R.mipmap.logo);
+    }
     public void htmlActivity(View view){
 
     }
